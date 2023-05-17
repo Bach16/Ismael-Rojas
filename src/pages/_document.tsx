2a2798e1f44f60015562c1ae09f4f2ef53eba13e
Bach16 <gwyn56717@gmail.com>
2023-05-16 20:57:09 -0500

    add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <meta charSet="uft-8"/>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Akshar&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
