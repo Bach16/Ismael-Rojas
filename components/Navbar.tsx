c59b325af867ec120261a85e1d01e4093336ec51
Bach16 <gwyn56717@gmail.com>
2023-05-19 01:28:54 -0500

    change color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../public/IR-2nd-white.png";
+import logo from "../public/Changed_color_no_BG.png";
 import {
   Drawer,
   DrawerBody,
@@ -17,18 +17,23 @@ import Link from "next/link";
 import { CgMenuRound } from "react-icons/cg";
 
 const Navbar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = React.useRef()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
   return (
-    <Flex className="pt-7 text-xl items-center justify-between ">
+    <Flex className="pt-7 text-xl items-center justify-between  ">
       <Link href="/" className="w-[200px]">
-      <Flex className=" justify-between items-center m-0 ">
-        <img src={logo.src} className="h-9  w-9.5" alt="logo" />
-      <Text className=" md:flex hidden font-medium text-2xl text-white font-inter">Ismael Rojas</Text>
-      </Flex>
+        <Flex className=" justify-between items-center m-0 ">
+          <img src={logo.src} className="h-9  w-9.5" alt="logo" />
+          <Text className=" md:flex hidden font-medium text-2xl text-white font-inter text-[#00C1B0]">
+            Ismael Rojas
+          </Text>
+        </Flex>
       </Link>
 
-      <CgMenuRound className="md:hidden font-thin text-white h-12 w-12"  onClick={onOpen} />
+      <CgMenuRound
+        className="md:hidden font-thin text-white h-12 w-12"
+        onClick={onOpen}
+      />
 
       <Drawer
         isOpen={isOpen}
@@ -41,28 +46,28 @@ const Navbar = () => {
         <DrawerContent>
           <DrawerCloseButton />
           <DrawerBody className="text-right bg-slate-900">
-            <Link href="/projects">
-              <Text>Projects</Text>
+            <Link href="/developer">
+              <Text>Developer</Text>
+            </Link>
+            <Link href="/designer">
+              <Text>Designer</Text>
             </Link>
             <Link href="/contact">
               <Text>Contact</Text>
-            </Link>
-            <Link href="/about">
-              <Text>About me</Text>
             </Link>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
 
-      <div className="md:flex hidden text-white justify-between mr-10 w-[315px] font-[360px]">
-        <Link href="/projects">
-          <Text>Projects</Text>
+      <div className="md:flex hidden text-white justify-between mr-10 w-[315px]  text-[#00C1B0] font-[360px]">
+        <Link href="/developer">
+          <Text>Developer</Text>
+        </Link>
+        <Link href="/designer">
+          <Text>Designer</Text>
         </Link>
         <Link href="/contact">
           <Text>Contact</Text>
-        </Link>
-        <Link href="/about">
-          <Text>About me</Text>
         </Link>
       </div>
     </Flex>
