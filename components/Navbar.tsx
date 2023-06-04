3e5fd74cbcc317e54407585acb1fca3de5313222
Bach16 <gwyn56717@gmail.com>
2023-06-04 16:16:36 -0500

    design refactor

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../public/Changed_color_no_BG.png";
 import {
+  Divider,
   Drawer,
   DrawerBody,
   DrawerCloseButton,
@@ -8,6 +9,7 @@ import {
   DrawerHeader,
   DrawerOverlay,
   Flex,
+  Icon,
   IconButton,
   Input,
   Text,
@@ -15,21 +17,22 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { CgMenuRound } from "react-icons/cg";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import DrawerItem from "./DrawerItem";
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
   return (
     <Flex className="pt-7 text-xl items-center justify-between  ">
-      <Link href="/" className="w-[200px]">
+      <Link href="/" className="w-[140px]">
         <Flex className=" justify-between items-center m-0 ">
           <img src={logo.src} className="h-9  w-9.5" alt="logo" />
-          
         </Flex>
       </Link>
 
       <CgMenuRound
-        className="md:hidden font-thin text-[#00C1B0] h-12 w-12"
+        className="md:hidden font-thin text-[#f2f2f2] h-12 w-12"
         onClick={onOpen}
       />
 
@@ -41,32 +44,39 @@ const Navbar = () => {
         size="xs"
       >
         <DrawerOverlay />
-        <DrawerContent>
+        <DrawerContent color="#f2f2f2" backgroundColor="#222">
+          <Flex className="justify-between p-[20px]">
+          <img src={logo.src} className="h-9  w-9.5" alt="logo" />
+
           <DrawerCloseButton />
-          <DrawerBody className="text-right bg-slate-900">
-            <Link href="/developer">
-              <Text>Developer</Text>
-            </Link>
-            <Link href="/designer">
-              <Text>Designer</Text>
-            </Link>
-            <Link href="/contact">
-              <Text>Contact</Text>
-            </Link>
+          </Flex>
+          <DrawerBody className="text-[30px]">
+            
+            <Divider bgColor="#00C1B0" color="#00C1B0" h="2px"/>
+            <DrawerItem path="/about" text="About"/>
+            <Divider bgColor="#00C1B0" color="#00C1B0" h="2px"/>
+            <DrawerItem path="/projects" text="Projects"/>
+            <Divider bgColor="#00C1B0" color="#00C1B0" h="2px"/>
+            <DrawerItem path="/contact" text="Contact"/>
+            <Divider bgColor="#00C1B0" color="#00C1B0" h="2px"/>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
 
-      <div className="md:flex hidden justify-between mr-10 w-[315px]  text-[#00C1B0] font-[360px]">
-        <Link href="/developer">
-          <Text>Developer</Text>
-        </Link>
-        <Link href="/designer">
-          <Text>Designer</Text>
+      <div className="md:flex hidden antialiased justify-between mr-[10px] w-[470px] font-[10] text-[20px] text-[#f2f2f2] font-[360px]">
+        <Link href="/projects">
+          <Text>Projects</Text>
         </Link>
         <Link href="/contact">
           <Text>Contact</Text>
         </Link>
+        <Link href="/about">
+          <Text>About</Text>
+        </Link>
+      </div>
+      <div className="md:flex hidden justify-between mr-[60px] w-[60px]  text-[#f2f2f2] font-[10]">
+          <Text>ES</Text>
+          <Text borderBottom="1px solid #00C1B0" color="#00C1B0">EN</Text>
       </div>
     </Flex>
   );
