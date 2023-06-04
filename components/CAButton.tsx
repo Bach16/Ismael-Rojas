f8ab03f67ae46f3c77f81aad9b43c8c8345692e5
Bach16 <gwyn56717@gmail.com>
2023-06-04 16:15:27 -0500

    design refacto

--- components/CAButton.tsx
+++ components/CAButton.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 const CAButton = (color: any) => {
   return (
-    <Link href="/projects">
+    <Link href="/projects" className=" md:hidden ">
       <Button
-        bgColor={color.color}
+        border="1px solid #f2f2f2"
         className=" font-['Inter'] md:w-44 md:h-10 md:text-[17] w-40 h-10 text-base rounded-full "
       >
         <Flex className="justify-between font-inter items-center">
-          <Text color="#222222"  fontWeight="600">
-            See projects
+          <Text color="#f2f2f2"  fontWeight="600">
+            Projects
           </Text>
         </Flex>
       </Button>
