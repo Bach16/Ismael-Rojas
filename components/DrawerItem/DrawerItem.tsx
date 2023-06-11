5bda937c33019b8c328b14aa873c60add9f3012c
Bach16 <gwyn56717@gmail.com>
2023-06-11 03:48:46 -0500

    change color

--- components/DrawerItem/DrawerItem.tsx
+++ components/DrawerItem/DrawerItem.tsx
@@ -8,9 +8,9 @@ import "./DrawerItem.module.css";
 const DrawerItem = (path: any) => {    
   return (
     <Link href={path.path}>
-      <Flex className={`asu justify-between items-center pr-[20px]  pl-[20px] h-[60px] font-black font-inter`}>
+      <Flex className={`asu justify-between items-center pr-[20px]  pl-[20px] h-[60px] font-bold font-inter`}>
         <Text className="animate__animated animate__fadeInLeft1 animate__delay-1s" >{path.text}</Text>
-        <Icon className="animate__animated animate__fadeInLeft2 animate__delay-1s" color="#00C1B0" as={BsFillArrowRightCircleFill} />
+        <Icon className="animate__animated animate__fadeInLeft2 animate__delay-1s" color="#D33367" as={BsFillArrowRightCircleFill} />
       </Flex>
     </Link>
   );
