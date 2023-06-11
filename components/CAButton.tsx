2c9b0e9ed33aff1cee964dee5a423158c79f1d6d
Bach16 <gwyn56717@gmail.com>
2023-06-11 03:48:35 -0500

    add responsive design

--- components/CAButton.tsx
+++ components/CAButton.tsx
@@ -3,15 +3,17 @@ import logo from "../public/IR-2nd-white.png";
 import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
-const CAButton = (color: any) => {
+const CAButton = (r: any) => {
+  console.log(r.r);
+  
   return (
-    <Link href="/projects" className=" md:hidden ">
+    <Link href="/projects" className={` ${r.r == true? "md:hidden" : "md:flex" }`}>
       <Button
-        border="1px solid #f2f2f2"
-        className=" font-['Inter'] md:w-44 md:h-10 md:text-[17] w-40 h-10 text-base rounded-full "
+        border="1px solid #222"
+        className="md:w-44 md:h-10 md:text-[17] w-40 h-10 text-[19px] tracking-wide antialiased rounded-full "
       >
-        <Flex className="justify-between font-inter items-center">
-          <Text color="#f2f2f2"  fontWeight="600">
+        <Flex className="justify-between items-center">
+          <Text color="#222"  fontWeight="500">
             Projects
           </Text>
         </Flex>
