dae39a8c268ecd76fb4b71df649763b3891322f8
Bach16 <gwyn56717@gmail.com>
2023-06-11 03:48:58 -0500

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,7 @@
 export { default as CAButton } from './CAButton';
+export { default as DrawerItem } from './DrawerItem/DrawerItem';
+export { default as Footer } from './Footer';
+export { default as NavItems } from './Navbar/NavItems';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as SideBar } from './SideBar';
 export { default as asd } from './asd';
