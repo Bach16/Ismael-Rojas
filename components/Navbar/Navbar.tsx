434ee277ccf9bfabb8478e5bfa5b29a2854f8c4c
Bach16 <gwyn56717@gmail.com>
2023-06-18 03:14:25 -0500

    design refactor

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../public/IR-2nd.png";
+import logo from "../../public/Icon-I.png";
 import {
   Divider,
   Drawer,
@@ -26,17 +26,20 @@ const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
   return (
-    <Flex className=" pt-3 md:pt-7 text-xl items-center justify-between  ">
+    <Flex className=" pt-3 md:pt-7 w-[100%] md:w-[595px] text-xl items-center ">
+      <div className=" w-[100%] justify-between md:justify-center flex md:hidden  ">
       <Link href="/" className="w-[140px]">
-        <Flex className=" justify-between items-center m-0 ">
-          <img src={logo.src} className="h-9  w-9.5" alt="logo" />
+        <Flex className="  justify-between items-center m-0 ">
+            <img src={logo.src} className="h-11  w-11.5" alt="logo" />
         </Flex>
       </Link>
 
       <CgMenuRound
-        className="md:hidden font-thin text-[#222] h-12 w-12"
+        className="font-thin text-[#222] h-12 w-12"
         onClick={onOpen}
       />
+      </div>
+
 
       <Drawer
         isOpen={isOpen}
@@ -48,28 +51,31 @@ const Navbar = () => {
         <DrawerOverlay />
         <DrawerContent color="#222" backgroundColor="#f2f2f2">
           <Flex className="justify-between p-[20px]">
-            <img src={logo.src} className="h-9  w-9.5" alt="logo" />
+            <img src={logo.src} className="h-11  w-11.5" alt="logo" />
 
             <DrawerCloseButton />
           </Flex>
           <DrawerBody className="text-[30px]">
+            
             <div className={style.diva} />
-            <DrawerItem path="/about" text="About" />
+            <DrawerItem path="/" text="Home" />
             <div className={style.diva2} />
             <DrawerItem path="/projects" text="Projects" />
             <div className={style.diva3} />
             <DrawerItem path="/contact" text="Contact" />
             <div className={style.diva4} />
+            <DrawerItem path="/about" text="About" />
+            <div className={style.diva5} />
           </DrawerBody>
         </DrawerContent>
       </Drawer>
       <NavItems />
-      <div className="md:flex hidden justify-between mr-[60px] w-[60px]  text-[#222] font-[400]">
+      {/* <div className="md:flex hidden justify-between  ml-[100px] w-[60px]  text-[#222] font-[400]">
         <Text>ES</Text>
-        <Text borderBottom="1px solid #D33367" color="#D33367">
+        <Text borderBottom="1px solid #07BEB8" color="#07BEB8">
           EN
         </Text>
-      </div>
+      </div> */}
     </Flex>
   );
 };
